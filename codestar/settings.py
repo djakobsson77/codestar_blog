d185bf9c90c761a0e4bff316921813696f99de6d
djakobsson77 <djakobsson77@gmail.com>
2026-04-13 09:38:47 +0200

    setting and retrieving environment variables

--- codestar/settings.py
+++ codestar/settings.py
@@ -15,6 +15,7 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
